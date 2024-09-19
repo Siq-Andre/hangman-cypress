63761d12a00e612cba5b568ac8572ca62aacdbae
André Siqueira <andre.siqueira@itlab.com.br>
2024-09-19 10:46:26 -0300

    Cypress finds score points

---
+++ cypress/e2e/scoreNumber.cy.js
@@ -0,0 +1,30 @@
+describe('check url access', () => {
+    it('check if the basic url is working', () => {
+      let arrayLetters = [];
+      let clickedLetters = new Set(); 
+   
+      cy.visit('/'); 
+   
+      cy.window().then((window) => {
+        cy.stub(window.console, 'log').callsFake((message) => {
+          if (Array.isArray(message) && message.every(char => typeof char === 'string')) {
+            arrayLetters = message;
+          }
+        });
+      });
+   
+      cy.wait(2500).then(() => {
+        expect(arrayLetters.length).to.be.greaterThan(0);
+   
+        arrayLetters.forEach((letra) => {
+          if (!clickedLetters.has(letra)) {
+            cy.get(`[data-test="letter-button-${letra}"]`).click();
+            clickedLetters.add(letra); 
+          }
+        });
+
+        cy.get(`[data-test="score"]`).should('exist').and('have.text', 'Pontuação: 1')
+
+      });
+    });
+  });
